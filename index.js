499f9d0de6e8177183643b543ca790787b9d4a4b
Roman Shtylman <shtylman@gmail.com>
2013-03-25 00:59:12 -0400

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var split = require('split');
+var through = require('through');
+
+module.exports = json;
+
+function json(transport) {
+
+  var input = through(function(data) {
+    transport.write(JSON.stringify(data) + '\n')
+  });
+
+  var output = through(function(data) {
+    try {
+      this.queue(JSON.parse(data))
+    }
+    catch (err) {
+      this.emit('error', err)
+    }
+  });
+
+  var out = transport.pipe(split()).pipe(output)
+
+  out.on('error', function(err) {
+    input.emit('error', err);
+  });
+
+  out.on('data', function(chunk) {
+    input.emit('data', chunk);
+  });
+
+  return input;
+}
